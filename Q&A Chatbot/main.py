d3e82bbacba2c750cd09cd68d68e7996185931f0
Vishal Kumar <vishalkumar.itcs@gmail.com>
2025-04-20 23:35:59 +0530

    commit

--- Q&A Chatbot/main.py
+++ Q&A Chatbot/main.py
@@ -1,6 +1,10 @@
 import os
 import streamlit as st
+from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
