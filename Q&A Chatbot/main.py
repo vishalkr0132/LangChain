a30383d5476a91fe6a788db0042b57e11c5d134a
Vishal Kumar <vishalkumar.itcs@gmail.com>
2025-04-20 23:43:04 +0530

    commit

--- Q&A Chatbot/main.py
+++ Q&A Chatbot/main.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -8,3 +9,22 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+# Load the GROQ API Key
+load_dotenv()
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+groq_api_key = os.getenv('groq_api_key')
+
+llm = ChatGroq(groq_api_key = groq_api_key,model="Llama3-8b-8192")
+
+prompt = ChatPromptTemplate.from_messages(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate respone based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+
+    """
+)
